33290a8728f497bd7598f1f003403adbdcbb4be9
Aníbal Pacheco <apacheco.uy@gmail.com>
2022-10-31 14:36:23 -0300

    context needed

--- utopia_cms_library/templatetags/library_tags.py
+++ utopia_cms_library/templatetags/library_tags.py
@@ -6,6 +6,7 @@ from utopia_cms_library.models import Book
 register = Library()
 
 
-@register.simple_tag()
-def book_list():
-    return loader.render_to_string('utopia_cms_library/book_list.html', {"object_list": Book.objects.all()})
+@register.simple_tag(takes_context=True)
+def book_list(context):
+    context.update({"object_list": Book.objects.all()})
+    return loader.render_to_string('utopia_cms_library/book_list.html', context.flatten())
