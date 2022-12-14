63108497be160122af0b63cf025934481cff8988
Aníbal Pacheco <apacheco.uy@gmail.com>
2022-12-14 02:55:07 -0300

    support to configure the elastic index name by settings

--- utopia_cms_library/apps.py
+++ utopia_cms_library/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import ugettext_lazy as _
 class UtopiaCmsLibraryConfig(AppConfig):
     name = "utopia_cms_library"
     verbose_name = _("library")
+    SEARCH_ELASTIC_INDEX_NAME = "utopia_cms_library"  # Name of the Elasticsearch index
--- utopia_cms_library/documents.py
+++ utopia_cms_library/documents.py
@@ -5,14 +5,15 @@ from builtins import object
 from django.conf import settings
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
-from utopia_cms_library.models import Book
+
+from .apps import UtopiaCmsLibraryConfig as library_settings
+from .models import Book
 
 
 @registry.register_document
 class BookDocument(Document):
     class Index(object):
-        # Name of the Elasticsearch index (TODO: use a better default name, for example "utopiacms_core_article")
-        name = getattr(settings, "SEARCH_ELASTIC_ARTICLES_INDEX_NAME", 'books')
+        name = library_settings.SEARCH_ELASTIC_INDEX_NAME
         # See Elasticsearch Indices API reference for available settings
         settings = {'number_of_shards': 1, 'number_of_replicas': 0}
 
