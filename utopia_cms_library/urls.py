04bdcdfc36cf5fa303ac0b82cbe1c029661fbb15
Aníbal Pacheco <apacheco.uy@gmail.com>
2022-12-23 20:12:12 -0300

    book sitemap wip

--- utopia_cms_library/urls.py
+++ utopia_cms_library/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
+from django.contrib.sitemaps.views import sitemap
 
 from .views import book_list, BookDetail
+from .sitemaps import BookSitemap
 
 
 urlpatterns = [
     url(r'^$', book_list, name="library-home"),
     url(r'^(?P<slug>[-\w]+)/$', BookDetail.as_view()),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': {"book": BookSitemap}}, name='library-book-sitemap'),
 ]
