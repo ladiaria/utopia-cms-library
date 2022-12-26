3428fe249808b03ce763c61010de44148df4fc96
Aníbal Pacheco <apacheco.uy@gmail.com>
2022-12-26 17:19:59 -0300

    better comment

--- utopia_cms_library/apps.py
+++ utopia_cms_library/apps.py
@@ -5,5 +5,4 @@ from django.utils.translation import ugettext_lazy as _
 class UtopiaCmsLibraryConfig(AppConfig):
     name = "utopia_cms_library"
     verbose_name = _("library")
-    # call this function path (if any) passing it a Book object and returns the result in Book.get_absolute_url
-    BOOK_URL_FUNCTION = None
+    BOOK_URL_FUNCTION = None  # If function path, Book.get_absolute_url will call with a Book obj and return the result
